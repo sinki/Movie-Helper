633bcab58d591f50ffef4240f25d6e0b140dc718
sinki.agarwal <sinki.agarwal@healthkart.com>
2016-05-15 23:44:28 +0530

    api key file

--- app/src/main/java/com/nanodegree/android/popmovies/utils/PopMovieAppConstants.java
+++ app/src/main/java/com/nanodegree/android/popmovies/utils/PopMovieAppConstants.java
@@ -1,11 +1,12 @@
-package com.nanodegree.android.popmovies;
+package com.nanodegree.android.popmovies.utils;
 
-public class PopMoviesAppConstants {
-
-  public static String MOVIE_DB_API_KEY = "my_api_key";
+/**
+ * Created by sinki.agarwal on 5/15/2016.
+ */
+public class PopMovieAppConstants {
   public static String MOVIE_DB_BASE_URL = "http://api.themoviedb.org/3/";
+  public static String MOVIE_DB_API_KEY = "my_api_key";
   public static String PICASSO_BASE_URL = "http://image.tmdb.org/t/p/";
   public static String PICASSO_THUMBNAIL_SIZE_SMALL = "w342";
   public static String PICASSO_THUMBNAIL_SIZE_BIG = "w500";
-
 }
